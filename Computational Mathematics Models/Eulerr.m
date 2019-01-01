75f79ff86c784618450355f9a180e37089fc2ed0
Adrian Gwozdziej <adrian.gwozdziej@meelogic.com>
2019-01-01 10:52:08 +0100

    add Euler method calculation

---
+++ Computational Mathematics Models/Eulerr.m
@@ -0,0 +1,33 @@
+% Newton’s law of cooling for a constant surrounding temperature is considered
+% so that the exact solution is known. The exact solution is compared with both
+% the Euler and improved Euler approximation solutions.
+
+%This code compares the discretization errors.
+
+% The Euler and improved Euler methods are used.
+maxk = 5; % number of time steps
+T = 50.0; % final time
+dt = T/maxk;
+time(1) = 0;
+u0 = 200.; % initial temperature
+c = 2./13.; % insulation factor
+usur = 70.; % surrounding temperature
+uexact(1) = u0;
+ueul(1) = u0;
+uieul(1) = u0;
+for k = 1:maxk %time loop
+	time(k+1) = k*dt;
+% exact solution
+	uexact(k+1) = usur + (u0 - usur)*exp(-c*k*dt);
+% Euler numerical approximation
+	ueul(k+1) = ueul(k) +dt*c*(usur - ueul(k));
+% improved Euler numerical approximation
+	utemp = uieul(k) +dt*c*(usur - uieul(k));
+    uieul(k+1)= uieul(k) + dt/2*(c*(usur - uieul(k))+c*(usur - utemp));
+	err_eul(k+1) = abs(ueul(k+1) - uexact(k+1));
+	err_im_eul(k+1) = abs(uieul(k+1) - uexact(k+1));
+end
+plot(time, ueul)
+maxk
+err_eul_at_T = err_eul(maxk+1)
+err_im_eul_at_T = err_im_eul(maxk+1)
